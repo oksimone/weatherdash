2ce1062fb34501c19cbba3841173e01c6c4c505c
ksimone <ksworthy@windstream.net>
2020-08-14 23:37:08 -0400

    final touches

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # weatherdash
+
+Weather Dashboard is assignment number 6 of my Full-Stack Bootcamp and this was extremely challenging. I'm still trying to wrap my head around utilizing functions, and using web APIs Even though I'm missing some parts of this assignment, I was able to tackle a good bit of this homework. Below are the requirements of the assignment.
+
+ - the requirements of this assignment was to:
+    - seach for a city an 
+      - when searching for a city, it presents current and future conditions for the city searched and is added to the search history
+    - it also presents the city name, date, the icon of the weather conditions, the temperature and the UV index
+    - the assignment should also show a 5-day forecarst that displays the date, an icon of the weather conditions, the temperature, and the humidity
+    - and, the assignement should show buttons of the previously searched city with the forecast of that city
+
+When I first started the assignment, I didn't think to code the HTML dynamically through javascript. So, I started hardcoding my sidebar and card decks into the HTML. Then realized later that coding it in a way that wasn't dynamic would have made the assignment much more difficult than it needed to be. The first couple of screenshots I took was when I was coding it not dynamically, and the last photo is my final product. 
+
+![](assets/screenshot2.png)
+![](assets/screenshot1.png)
+
+
+
--- assets/script.js
+++ assets/script.js
@@ -3,81 +3,86 @@
 // var cityName = [];
 
 
-$("#find-city").on("click", function(event) {
+$("#find-city").on("click", function (event) {
     event.preventDefault();
     var city = $("#city-search").val();
     var weatherAPIKey = "c27e17b2075378ac79414f96f80ea46b";
     var weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + weatherAPIKey
     console.log(city)
-    
+
     $.ajax({
         url: weatherQueryURL,
         method: "GET",
-    }).then(function(response) {
-        
+    }).then(function (response) {
+
         var results = response.data;
         console.log(weatherQueryURL)
-        
+
         var far = (response.main.temp - 273.15) * 1.80 + 32
         $(".city").text(city.toUpperCase() + " " + (moment().format("M/D/YYYY")));
         $(".temp").text("Temperature " + far.toFixed(2))
-        $(".humidity").text("Humidity: " + response.main.humidity )
+        $(".humidity").text("Humidity: " + response.main.humidity)
         $(".windspeed").text("Windspeed: " + response.wind.speed)
         var cityName = []
         var lat = response.coord.lat
         var lon = response.coord.lon
-        var iconMain = "http://api.openweathermap.org/img/w/" + response.weather[0].icon +".png";
+        var iconMain = "http://api.openweathermap.org/img/w/" + response.weather[0].icon + ".png";
         $("#iconMain").attr("src", iconMain);
-        
-        function uvI (){
+
+        function uvI() {
             var uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=c27e17b2075378ac79414f96f80ea46b&lat=" + lat + "&lon=" + lon;
             $.ajax({
                 url: uvURL,
                 method: "GET"
-            }) .then(function(response2){
+            }).then(function (response2) {
                 console.log(uvURL)
                 var uv = response2.value;
                 $(".uv").text("UV Index " + uv);
+                $(".uv").css("color", "red")
+               
             })
         }
         uvI()
-        }
-      );
+    }
+    );
 
-      $.get("https://api.openweathermap.org/data/2.5/forecast?q=" + city +"&appid=" + weatherAPIKey)
-      .then(function(res){
-          var k = 1;
-          for(var i = 0; i < res.list.length; i++){
-              var curr = res.list[i]
-              if(curr.dt_txt.includes("12:00")){
-               var cardDiv =  $("<div>").attr("class", "card card bg-primary text-white");
-               $(".card-deck").append(cardDiv);
-               var date = $("<h6>").text((moment().add(k, "days").format("MM/DD/YYYY")));
-               cardDiv.append(date);
-               k++;
-               var far2 = (curr.main.temp - 273.15) * 1.80 + 32
-               var p1 = $("<p>").text("Temperature " + far2.toFixed(2))
-               cardDiv.append(p1)
-                var p2 = $("<p>").text("Humidity " +curr.main.humidity)
-                cardDiv.append(p2);
+    $.get("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + weatherAPIKey)
+        .then(function (res) {
+            var k = 1;
+            for (var i = 0; i < res.list.length; i++) {
+                var curr = res.list[i]
+                if (curr.dt_txt.includes("12:00")) {
+                    var cardDiv = $("<div>").attr("class", "card card bg-primary text-white");
+                    $(".card-deck").append(cardDiv);
+                    var date = $("<h6>").text((moment().add(k, "days").format("MM/DD/YYYY")));
+                    cardDiv.append(date);
+                    k++;
+                    var far2 = (curr.main.temp - 273.15) * 1.80 + 32
+                    var p1 = $("<p>").text("Temperature " + far2.toFixed(2))
+                    cardDiv.append(p1)
+                    var p2 = $("<p>").text("Humidity " + curr.main.humidity)
+                    cardDiv.append(p2);
+                    var img2 = $("<img>").attr("src", "http://api.openweathermap.org/img/w/" + curr.weather[0].icon + ".png");
+                    img2.css('width', '50px')
+                    img2.css('height', '50px')
 
-                
-                var iconMain2 = "http://api.openweathermap.org/img/w/" + response.weather[0].icon +".png";
-        $("#iconMain").attr("src", iconMain);
-                  console.log(curr)
-                //   curr.push(cards)
-                
-  
-  
-              }
-          }
-  
-      })
+                    
+                    cardDiv.append(img2)
+
+                    console.log(curr)
+                    //   curr.push(cards)
 
 
 
+                }
+            }
+
+        })
 
 
 
 
-  });
+
+
+
+});
